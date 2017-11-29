b5c45f47cbfd2cfd9fe9841996ea062a77bef3ea
Huw Taylor <huw_taylor@hotmail.co.uk>
2017-11-29 18:27:00 +0000

    added character and debug actions

--- main.lua
+++ main.lua
@@ -1,22 +1,12 @@
-local p = print
-local msg = {}
-print = function(...) 
-    p(...)
-    local arg = {...}
-    local line = ""
-    for _, a in ipairs(arg) do
-        line = line .. a .. "\n"
-    end
-    table.insert(msg, line)
-    DEBUG.print = function()
-        love.graphics.print(msg, 460, 0)
-    end
-end
-
-
 local pathfinder = require 'lib_pathfinder'
 
-DEBUG = {}
+local guard = require 'cls_guard'
+local actor = require 'cls_character'
+
+DEBUG = {
+    drawables = {},
+    messages  = {},
+}
 
 function love.load()
     map = {
@@ -26,46 +16,76 @@ function love.load()
         {24, 96, 32, 190, 128, 128},
         {244, 140, 270, 190, 350, 160, 300, 50},
         {244, 140, 190, 165, 210, 215, 270, 190},
+        {32, 190, 60, 256, 160, 160, 128, 128},
         -- {},
     }
     pathfinder.map(map)
-    path_start = nil
-    path_end = nil
-    timer = 0
-    demo_timer = 0
-    demo_stage = 0
-    path = nil
+    guards = {
+        guard.new({
+            name = "foo",
+            position = {80, 80},
+            direction = -1,
+            patrol = {
+                {170, 40}, {250, 70}, {80, 80}, 
+            },
+        }),
+    }
+    party = {
+        actor.new({
+            name = "bar",
+            position = {230, 180},
+            direction = 0,
+        }),
+    }
+    action = nil
 end
 
 function love.update(dt)
-    timer = timer + dt
+    for _, g in pairs(guards) do 
+        g:update(dt, map) 
+    end
+    for _, g in pairs(party) do 
+        g:update(dt, map) 
+    end
 end
 
 function love.mousepressed(mx, my, key)
-    if key == 1 then
-        path_start = {mx, my}
-    end
     if key == 2 then
-        path_end = {mx, my}
+        local path = pathfinder.path(party[1].position, {mx, my})
+        if path then
+            party[1].path = path
+        end
+    end    
+
+    if key == 1 and action then
+        if action == "noise" then
+            
+        elseif action == "teleport" then
+            party[1].position = {mx, my}
+        elseif action == "sighting" then
+            
+        elseif action == "distraction" then
+
+        end
+        action = nil
     end
-    path = nil
-    demo_stage = 0
 end
 
 function love.keypressed(key)
-    if key == "space" and path_start and path_end then
-        if not path then
-            path = pathfinder.path(path_start, path_end)
-        else
-            demo_stage = math.min(demo_stage + 1, #DEMONSTRATION)
-            print(demo_stage .. "/" .. #DEMONSTRATION)
-        end
+    if key == "escape" then
+        party[1].path = nil
+    end
+
+
+    if key == "n" then
+        action = "noise"
+    end
+    if key == "t" then
+        action = "teleport"
     end
 end
 
-function love.draw()
-    local mx, my = love.mouse.getPosition()
-
+local function draw_map(map)
     for _, shape in pairs(map) do
         love.graphics.setColor(128, 255, 255, 32)
         love.graphics.polygon("fill", unpack(shape))
@@ -76,40 +96,29 @@ function love.draw()
             love.graphics.print(math.floor(i/2), shape[i], shape[i+1] - 8)
         end
     end
+end
 
-    if path_start then
-        love.graphics.circle("line", path_start[1], path_start[2], (timer * 6) % 4)
+function love.draw()
+    local mx, my = love.mouse.getPosition()
 
-        if love.keyboard.isDown("lshift") then
-            love.graphics.line(path_start[1], path_start[2], mx, my)
-        end
+    draw_map(map)
+
+    for _, g in pairs(guards) do 
+        g:draw() 
+    end
+    for _, g in pairs(party) do 
+        g:draw() 
     end
 
-    if path_end then
-        love.graphics.circle("line", path_end[1], path_end[2], (6 - timer * 6) % 4)
+    if action then
+        love.graphics.setColor(255, 255, 255)
+        love.graphics.print(action, 255, 0)
     end
 
-    if path then
-        for i = 1, #path-1 do
-            love.graphics.line(path[i][1], path[i][2], path[i+1][1], path[i+1][2])
-        end 
+    for _, d in pairs(DEBUG.drawables) do
+        love.graphics.setColor(255, 255, 255)
+        d()
     end
-
-    if demo_stage > 0 then
-        local s1, s2, vl1, vl2, vr1, vr2, nl1, nl2, nr1, nr2 = unpack(DEMONSTRATION[demo_stage])
-        love.graphics.setColor(255, 0, 0)
-        love.graphics.line(s1, s2, vl1, vl2)
-        love.graphics.setColor(0, 255, 0)
-        love.graphics.line(s1, s2, vr1, vr2)
-        love.graphics.setColor(128, 0, 0)
-        love.graphics.line(s1, s2, nl1, nl2)
-        love.graphics.setColor(0, 128, 0)
-        love.graphics.line(s1, s2, nr1, nr2)
-    end
-
-    -- for _, draw in pairs(DEBUG) do
-    --     draw()
-    -- end
 
     love.graphics.setColor(255, 255, 255)
     love.graphics.print(mx .. ", " .. my, 0, 0)
