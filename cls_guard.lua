a3e2d4c1c8e3711be94db68196430048af37c2eb
Huw Taylor <huw_taylor@hotmail.co.uk>
2017-11-30 23:54:53 +0000

    guards can hear noises and look from side to side

--- cls_guard.lua
+++ cls_guard.lua
@@ -1,37 +1,38 @@
 local pathfinder = require 'lib_pathfinder'
 
-local person = require 'cls_person'
+local Person = require 'cls_person'
+local Actor  = require 'cls_character'
 
-local guard = {}
-setmetatable(guard, person)
-guard.__index = guard
+local Guard = {}
+setmetatable(Guard, Person)
+Guard.__index = Guard
 
-function guard.new(options)
-    local this = person.new(options)
-    setmetatable(this, guard)
+function Guard.new(options)
+    local this = Person.new(options)
+    setmetatable(this, Guard)
 
-    this.viewcones = {
-        {
-            width = math.pi / 16,
-            range = 64,
-            focus = true,
-        },
-    } -- @TODO: make this dependent on options
-    this.patrol = options.patrol or nil
-    this.patrol_stage = 0
+    this.viewcones   = options.viewcones    or {}
+    this.watch_width = options.watch_width or math.pi / 4
+    this.watch_speed = options.watch_speed or math.pi / 2
+
+    this.watch_direction = this.direction
+    this.watch_timer     = 0
+    this.behaviour       = "patrol"
+
+    this.max_view_distance = 0
+    for _, v in pairs(this.viewcones) do
+        if v.range > this.max_view_distance then
+            this.max_view_distance = v.range
+        end
+    end
 
     return this
 end
 
-function guard:speed()
-    -- @TODO: have different speeds (also include stuff like carrying bodies, etc.)
-    -- if self.is_crouching then
-    --     return self.speed_crouching
-    -- else 
-    --     return self.speed_walking
-    -- end
-    return 64
-end 
+function Guard:hear_noise(x, y)
+    print("heard a noise.")
+    self.behaviour = "suspicious"
+end
 
 local function update_movement(self, dt)
     local next_point = self.path[1]
@@ -41,37 +42,59 @@ local function update_movement(self, dt)
             self.path = nil
         end
     else
-        local x, y = self:get_position()
-        local dx = next_point[1] - x
-        local dy = next_point[2] - y
-        local r = math.atan2(dy, dx)
-        local mx = self:speed() * dt * math.cos(r)
-        local my = self:speed() * dt * math.sin(r)
-        self.position = {x + mx, y + my}
+        self:move_towards(next_point[1], next_point[2], self:speed(), dt)
     end
 end
 
-function guard:update(dt)
-    if self.patrol and not self.path then
-        self.patrol_stage = self.patrol_stage + 1
-        if self.patrol_stage > #self.patrol then
-            self.patrol_stage = 1
+local function update_watch(self, dt, scene)
+    self.watch_timer = self.watch_timer + dt
+    self.watch_direction = self.watch_width * math.sin(self.watch_timer * self.watch_speed)
+
+    local objects = scene:objects_around_point(self.position, self.max_view_distance)
+    if #objects > 0 then
+        for _, obj in pairs(objects) do
+            if getmetatable(obj) == Actor then
+
+                -- @TODO: make sure object is in cone (rather than distance) and can be seen.
+                self.behaviour = "suspicious"
+            end
         end
-        self.path = pathfinder.path(self.position, self.patrol[self.patrol_stage])
-    end
-    if self.path then
-        update_movement(self, dt)
+    else
+        self.behaviour = "patrol"
     end
 end
 
-function guard:draw_viewcones()
+function Guard:update(dt, scene)
+    if self.behaviour == "patrol" then
+        if self.path then
+            update_movement(self, dt)
+        end
+        update_watch(self, dt, scene)
+    elseif self.behaviour == "suspicious" then
+        update_watch(self, dt, scene)
+    end
+end
+
+function Guard:draw_vision()
     for _, viewcone in pairs(self.viewcones) do
         -- @TODO: draw an arc, filled
+        if viewcone.focus then
+            love.graphics.setColor(32, 255, 32, 128)
+        else
+            love.graphics.setColor(32, 255, 32, 96)
+        end
+        local r1 = self.watch_direction - viewcone.width / 2
+        local r2 = self.watch_direction + viewcone.width / 2
+        love.graphics.arc("fill", self.position[1], self.position[2], viewcone.range, r1, r2)
+        if self.behaviour == "suspicious" and viewcone.focus then
+            love.graphics.setColor(255, 32, 32, 128)
+            love.graphics.arc("fill", self.position[1], self.position[2], viewcone.range, r1, r2)
+        end
     end
 end
 
-function guard:draw()
-    person.draw(self)
+function Guard:draw()
+    Person.draw(self)
 
     if DEBUG then
         if self.patrol then
@@ -93,4 +116,4 @@ function guard:draw()
     end
 end
 
-return guard
+return Guard
