7b74add45c09d2cf3f09488c27ca5d4375c1b9c7
Huw Taylor <huw_taylor@hotmail.co.uk>
2017-11-30 23:56:38 +0000

    added config and library files for further down the line

---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(game)
+    game.window.title  = "Sapphire Shadows"
+    game.window.width  = 640
+    game.window.height = 480
+end
---
+++ lib_tlo.lua
@@ -0,0 +1,135 @@
+local tlo = {
+    _VERSION     = 'v0.0.1',
+    _DESCRIPTION = 'A Lua localisation library for LÖVE games',
+    _URL         = '',
+    _LICENSE     = [[
+        MIT License
+
+        Copyright (c) 2017 Huw Taylor
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+    ]]
+}
+
+tlo.actions = {
+    ERROR        = "error",
+    IGNORE       = "ignore",
+    RETURN_BLANK = "blank",
+    RETURN_NIL   = "nil",
+}
+
+tlo.settings = {
+    -- BLANK, ERROR, IGNORE, NIL
+    onMissingLocalisation   = tlo.actions.IGNORE,
+    addMissingLocalisations = true,
+    -- BLANK, ERROR, IGNORE, NIL
+    onUnsetLanguage         = tlo.actions.IGNORE,
+    -- ERROR, IGNORE
+    onMissingLanguageFile   = tlo.actions.ERROR,
+    addMissingLanguageFiles = true,
+}
+
+local currentLanguage = nil
+local languageFilesPath = "lang"
+local lookupTable = {}
+
+local function addString(newString)
+    local files = love.filesystem.getDirectoryItems(languageFilesPath)
+    for _, file in pairs(files) do
+        local path = languageFilesPath .. "/" .. file
+        local fileString = love.filesystem.read(path)
+        local index = fileString:find("}[^}]*$")
+        local newString = newString:gsub("\n", "\\n")
+        local newContent = fileString:sub(1, index - 1) ..
+                           "    [\"" .. newString .. "\"] = \"" .. newString .. "\", -- AUTOMATICALLY ADDED.\n" ..
+                           fileString:sub(index)
+        love.filesystem.write(path, newContent)
+    end
+end
+
+function tlo.localise(string)
+    if not currentLanguage then
+        if tlo.settings.onUnsetLanguage == tlo.actions.RETURN_BLANK then
+            return ""
+        elseif tlo.settings.onUnsetLanguage == tlo.actions.ERROR then
+            error("There is no language set. Use tlo.setLanguage() to set which language to use.")
+        elseif tlo.settings.onUnsetLanguage == tlo.actions.IGNORE then
+            return string
+        elseif tlo.settings.onUnsetLanguage == tlo.actions.RETURN_NIL then
+            return nil
+        end
+    else
+        if lookupTable[string] then
+            return lookupTable[string]
+        else
+            if tlo.settings.addMissingLocalisations then
+                addString(string)
+            end
+            if tlo.settings.onMissingLocalisation == tlo.actions.RETURN_BLANK then
+                return ""
+            elseif tlo.settings.onMissingLocalisation == tlo.actions.ERROR then
+                error("Missing localisation for '" .. string .. "' in " .. currentLanguage .. ".")
+            elseif tlo.settings.onMissingLocalisation == tlo.actions.IGNORE then
+                return string
+            elseif tlo.settings.onMissingLocalisation == tlo.actions.RETURN_NIL then
+                return nil
+            end
+        end
+    end
+end
+
+function tlo.deferredLocalise(string)
+    return function()
+        return tlo.localise(string)
+    end
+end
+
+function tlo.setLanguage(languageCode)
+    local path = languageFilesPath .. "/" .. languageCode
+    local exists = love.filesystem.exists(path)
+    if exists then
+        lookupTable = love.filesystem.load(path)()
+    else
+        if tlo.settings.addMissingLanguageFiles then
+            local f = love.filesystem.newFile(path)
+            f:open('r')
+            f:close()
+        end
+        if tlo.settings.onMissingLanguageFile == tlo.actions.ERROR then
+            error("Missing language file '" .. languageCode .. "'.")
+        elseif tlo.settings.onMissingLanguageFile == tlo.actions.IGNORE then
+            lookupTable = lookupTable or {}
+        end
+    end
+    currentLanguage = languageCode
+end
+
+function tlo.getLanguage()
+    return currentLanguage
+end
+
+function tlo.setLanguagesFolder(path)
+    languageFilesPath = path
+end
+
+function tlo.getLanguagesFolder(path)
+    return languageFilesPath
+end
+
+return tlo
